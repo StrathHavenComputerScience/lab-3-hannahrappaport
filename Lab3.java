c092fc000cde4158fdd72203322237b6075e480b
hannahrappaport <51413749+hannahrappaport@users.noreply.github.com>
2019-06-10 07:47:10 -0400

    Update Lab3.java

--- Lab3.java
+++ Lab3.java
@@ -16,7 +16,52 @@ public class Lab3
     
   public static void lightCandles()
   {
-    //insert instructions below
+    candleNspace();
+        candleNspace();
+        candleNspace();
+        candleNspace();
+        candleNspace();
+        candleNspace();
+        candleNspace();
+        candleNspace();
+        candleNspace();
+        candleNspace();
+    }
+
+    public static void candleNspace()
+    //lights the candle and moves to the correct space to start the next one
+    //pre: facing right post: also facing right
+    {oneCandle();
+        Robot.move();
+        turnRight();
+        Robot.move();
+        if(Robot.frontIsClear() == true)
+        {Robot.move();}
+        Robot.turnLeft();}
+
+    public static void oneCandle()
+    //lights just one candle and remains on top of candle
+    //pre: facing right post: facing right, square is darkened
+    {if (Robot.frontIsClear() == false) 
+        {Robot.turnLeft();
+            Robot.move();
+            turnRight();
+            if (Robot.frontIsClear() == false) {
+                Robot.turnLeft();
+                Robot.move();
+                turnRight();
+                Robot.move();
+                Robot.makeDark();
+            }else{
+                Robot.move();
+                Robot.makeDark();}}
+        else {while(Robot.frontIsClear() == true) {Robot.move();}}
+    }
+
+    public static void turnRight()
+    { Robot.turnLeft();
+        Robot.turnLeft();
+        Robot.turnLeft();}
   
   
   
@@ -41,7 +86,127 @@ public class Lab3
   //Complete this method.  You will need to write additional helper methods.
   public static void completeRoom()
   {
-    //insert instructions below
+   wholeRow();
+        wholeRow();
+        wholeRow();
+        wholeRow();
+    }
+    public static void oneSquare()
+    {
+        Robot.turnLeft();
+        if (Robot.frontIsClear() == true)
+        { Robot.move();
+            if(Robot.onDark() == false) {
+            Robot.makeDark();}
+            Robot.turnLeft();
+            Robot.turnLeft();
+            Robot.move();
+            Robot.turnLeft();
+            if (Robot.frontIsClear() == true) {
+            Robot.move();}}
+        else{ turnRight(); 
+            if (Robot.frontIsClear() == true) {
+            Robot.move();}
+            else{turnRight(); Robot.move(); Robot.turnLeft();}}}
+    public static void wholeRow()
+    {  
+        oneSquare();
+        oneSquare();
+        oneSquare();
+        oneSquare();
+        oneSquare();
+        turnRight(); }
+            //Run this method to test swapAll on map swap1.txt
+    public static void testSwapAll1()
+    {
+        Robot.load("swap1.txt");
+        Robot.setDelay(0.05);
+        swapAll();
+    }
+   
+    //Run this method to test swapAll on map swap2.txt
+    public static void testSwapAll2()
+    {
+        Robot.load("swap2.txt");
+        Robot.setDelay(0.05);
+        swapAll();
+    }
+
+    //Complete this method.  You will need to write additional helper methods.
+    public static void swapAll()
+    { checkarow();
+        turnRight();
+   
+        checkotherrow();
+  
+    }
+    public static void checkotherrow()
+    { Robot.move();
+        if(Robot.onDark() == false) {
+            Robot.makeDark();}
+            else {Robot.makeLight();}
+            turnRight();
+            turnRight();
+            Robot.move();
+            Robot.turnLeft();
+            Robot.move();
+            checkothersquare();
+        checkothersquare();
+        checkothersquare();
+        checkothersquare();
+        checkothersquare();
+        checkothersquare();
+        checkothersquare();
+        checkothersquare();
+        Robot.turnLeft();
+        Robot.move();
+        if(Robot.onDark() == false) {
+            Robot.makeDark();}
+            else {Robot.makeLight();}}
+         
+        
+    public static void checkothersquare()
+    { Robot.turnLeft();
+        Robot.move();
+        if(Robot.onDark() == false) {
+            Robot.makeDark();}
+            else {Robot.makeLight();}
+            Robot.turnLeft();
+            Robot.turnLeft();
+            Robot.move();
+            Robot.turnLeft();
+            Robot.move();}
+    public static void checkasquare()
+    { Robot.turnLeft();
+        Robot.move();
+        if(Robot.onDark() == false) {
+            Robot.makeDark();}
+            else {Robot.makeLight();}
+            Robot.turnLeft();
+            Robot.turnLeft();
+            Robot.move();
+            Robot.turnLeft();
+            Robot.move();}
+    public static void checkarow()
+    { checkasquare();
+        checkasquare();
+        checkasquare();
+        checkasquare();
+        checkasquare();
+        checkasquare();
+        checkasquare();
+        checkasquare();
+        checkasquare();
+        Robot.turnLeft();
+        Robot.move();
+        if(Robot.onDark() == false) {
+            Robot.makeDark();}
+            else {Robot.makeLight();}
+            Robot.turnLeft();
+            Robot.turnLeft();
+            Robot.move();
+            Robot.turnLeft();
+        }
   
   
   
